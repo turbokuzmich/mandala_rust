e7e3446ba96fa99602f1330e987da864253a14bd
turbokuzmich <kurteev.d@yandex.ru>
2025-10-25 18:34:40 +0300

    v1.0

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
 mod calculation;
 
 use calculation::calculate_mandala;
-use iced::mouse;
-use iced::widget::canvas::{Cache, Canvas, Geometry, Program};
+use iced::widget::canvas::{Cache, Canvas, Geometry, Program, Text};
 use iced::widget::{TextInput, button, column, container, text};
 use iced::{
-    Color, Element, Fill, Point, Rectangle, Renderer, Result as IcedResult, Theme, alignment,
+    Color, Element, Fill, Pixels, Point, Rectangle, Renderer, Result as IcedResult, Size, Theme,
+    alignment, color,
 };
+use iced::{Vector, mouse};
+
+static COLORS: [Color; 9] = [
+    color!(0xE6194B),
+    color!(0xF58231),
+    color!(0xFFE119),
+    color!(0xBFEF45),
+    color!(0x3CB44B),
+    color!(0x42D4F4),
+    color!(0x4363D8),
+    color!(0x911EB4),
+    color!(0xF032E6),
+];
+
+static TEXT_SIZE: Pixels = Pixels(24.0);
 
 #[derive(Debug, Clone)]
 enum Message {
     Type(String),
     Submit,
+    Return,
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -27,7 +43,6 @@ struct Mandala<'a> {
 
 impl<'a> Mandala<'a> {
     fn new(result: &'a Vec<Vec<u16>>) -> Self {
-        println!("new: {:?}", result);
         Self {
             result,
             cache: Cache::default(),
@@ -47,8 +62,38 @@ impl<'a> Program<Message> for Mandala<'a> {
         _cursor: mouse::Cursor,
     ) -> Vec<Geometry<Renderer>> {
         let geometry = self.cache.draw(renderer, bounds.size(), |frame| {
-            frame.fill_rectangle(Point::new(0.0, 0.0), frame.size(), Color::WHITE);
+            let side = f32::min(frame.width(), frame.height());
+            let padding_x = (frame.width() - side) / 2.0;
+            let padding_y = (frame.height() - side) / 2.0;
+            let block_size = side / 16.0;
+            let block_half_size = block_size / 2.0;
+            let text_offset: Vector = [block_half_size, block_half_size].into();
+
+            for row in 0..16 {
+                let block_padding = (side - (row as f32 + 1.0) * block_size) / 2.0;
+                for col in 0..row + 1 {
+                    let x = padding_x + block_padding + (col as f32) * block_size;
+                    let y = padding_y + ((15 - row) as f32) * block_size;
+                    let index = self.result[15 - row][col];
+                    let color = COLORS.get((index - 1) as usize).unwrap();
+                    let top_left_point = Point::new(x, y);
+
+                    let text = Text {
+                        content: index.to_string(),
+                        position: top_left_point + text_offset,
+                        color: Color::BLACK,
+                        horizontal_alignment: alignment::Horizontal::Center,
+                        vertical_alignment: alignment::Vertical::Center,
+                        size: TEXT_SIZE,
+                        ..Text::default()
+                    };
+
+                    frame.fill_rectangle(top_left_point, Size::new(block_size, block_size), *color);
+                    frame.fill_text(text);
+                }
+            }
         });
+
         vec![geometry]
     }
 }
@@ -69,6 +114,11 @@ impl State {
             Message::Submit => {
                 self.screen = Screen::Result;
                 self.calculation = calculate_mandala(&self.input);
+            }
+            Message::Return => {
+                self.screen = Screen::Input;
+                self.input = "".to_string();
+                self.calculation = Err("Введите текст для мандалы".to_string());
             }
         }
     }
@@ -106,9 +156,11 @@ impl State {
                 Ok(result) => container(
                     column![
                         text(&self.input).size(20),
-                        Canvas::new(Mandala::new(result)).width(Fill).height(Fill)
+                        Canvas::new(Mandala::new(result)).width(Fill).height(Fill),
+                        button("Назад").on_press(Message::Return),
                     ]
                     .width(Fill)
+                    .spacing(10)
                     .align_x(alignment::Horizontal::Center),
                 )
                 .height(Fill)
