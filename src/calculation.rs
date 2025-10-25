8d95a6be4237a216e3045f3e93deb8da0a0ccff5
turbokuzmich <kurteev.d@yandex.ru>
2025-10-25 09:55:12 +0300

    Fix calculation

--- src/calculation.rs
+++ src/calculation.rs
@@ -23,32 +23,61 @@ fn get_sum(index_a: u16, index_b: u16) -> u16 {
 }
 
 pub fn calculate_mandala(text: &str) -> Result<Vec<Vec<u16>>, String> {
-    let mut indexes: Vec<u16> = text.chars().fold(vec![], |mut acc, letter| {
-        let lower_letter = letter.to_lowercase().next().unwrap();
-        let index = SYMBOL_MAP.get(&lower_letter);
-        if let Some(&index) = index {
-            acc.push(index);
-        }
-        acc
-    });
+    let mut indexes_a = text
+        .chars()
+        .map(|letter| SYMBOL_MAP.get(&letter.to_lowercase().next().unwrap()))
+        .filter(|index| index.is_some())
+        .map(|index| *(index.unwrap()))
+        .collect::<Vec<u16>>();
 
-    if indexes.len() < 2 {
+    if indexes_a.len() < 2 {
         return Err("Слишком короткий текст".to_string());
     }
 
-    while indexes.len() < 8 {
-        let last_index = indexes.last().unwrap();
-        let second_last_index = indexes.get(indexes.len() - 2).unwrap();
+    while indexes_a.len() < 8 {
+        let last_index = indexes_a.last().unwrap();
+        let second_last_index = indexes_a.get(indexes_a.len() - 2).unwrap();
         let sum = get_sum(*last_index, *second_last_index);
-        indexes.push(sum);
+        indexes_a.push(sum);
     }
 
-    // FIXME reduce to a row of size 8
+    let mut size = indexes_a.len();
+    let mut iteration: u16 = 0;
+    let mut indexes_b: Vec<u16> = vec![0; size];
 
-    let mut result: Vec<Vec<u16>> = Vec::with_capacity(indexes.len());
-    let size = indexes.len();
+    while size > 8 {
+        let (from, to) = if iteration % 2 == 0 {
+            (&mut indexes_a, &mut indexes_b)
+        } else {
+            (&mut indexes_b, &mut indexes_a)
+        };
 
-    for index in 1..size {
+        for i in 1..size {
+            to[i - 1] = get_sum(from[i - 1], from[i]);
+        }
+
+        size -= 1;
+        iteration += 1;
+    }
+
+    let reduced = if iteration % 2 == 0 {
+        &indexes_a
+    } else {
+        &indexes_b
+    };
+
+    let line = reduced
+        .iter()
+        .take(8)
+        .chain(reduced.iter().take(8).rev())
+        .map(|&index| index)
+        .collect::<Vec<u16>>();
+
+    let mut result: Vec<Vec<u16>> = Vec::with_capacity(16);
+
+    result.push(line);
+
+    for index in 1..16 {
         let previous = result.get(index - 1).unwrap();
 
         let mut row: Vec<u16> = Vec::with_capacity(previous.len() - 1);
